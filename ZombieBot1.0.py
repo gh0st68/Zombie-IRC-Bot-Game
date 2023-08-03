0c99039231c03e62666058a2143e8490dcae8939
gh0st <75190364+gh0st68@users.noreply.github.com>
2023-08-03 15:46:33 -0700

    Update ZombieBot1.0.py

--- ZombieBot1.0.py
+++ ZombieBot1.0.py
@@ -1,28 +1,40 @@
 #Made by: gh0st
 #Visit us @ IRC.TWISTEDNET.ORG CHANNELS: #TWISTED #DEV
 
-
 import ssl
 import irc.bot
 import irc.connection
 import time
 import random
-import sched
 import json
-import os
-import threading
 from jaraco.stream import buffer
+import threading
 
 class ZombieBot(irc.bot.SingleServerIRCBot):
-    def __init__(self, channel, nickname, server, port=6697):
+    def __init__(self, channel_list, nickname, server, port=6697):
         irc.client.ServerConnection.buffer_class = buffer.LenientDecodingLineBuffer
         factory = irc.connection.Factory(wrapper=ssl.wrap_socket)
         irc.bot.SingleServerIRCBot.__init__(self, [(server, port)], nickname, nickname, connect_factory=factory)
-        self.channel = channel
-        self.zombies = {}
-        self.scores = self.load_scores()
-        self.bullets = {}  
-        self.scheduler = sched.scheduler(time.time, time.sleep)
+        self.channel_list = channel_list
+        self.zombies = {channel.lower(): {} for channel in channel_list}
+        self.explosion_chance = 0.05
+        self.boss_chance = 0.10
+        self.boss_health = 3
+        self.scores = {}
+        self.bullets = {}
+        self.original_channels = channel_list.copy()
+
+        try:
+            with open("killz", "r") as f:
+                data = json.load(f)
+                self.scores = data.get('scores', {})
+                self.bullets = data.get('bullets', {})
+        except FileNotFoundError:
+            pass
+
+        self.channel_last_cleanup = {channel: 0 for channel in channel_list}
+        self.is_running = False
+        self.game_threads = []
 
     def is_connected(self):
         return self.connection.is_connected()
@@ -31,98 +43,170 @@ class ZombieBot(irc.bot.SingleServerIRCBot):
         c.nick(c.get_nickname() + "_")
 
     def on_welcome(self, c, e):
-        c.join(self.channel)
-        self.start_scheduler()
+        for channel in self.channel_list:
+            c.join(channel)
 
-    def start_scheduler(self):
-        thread = threading.Thread(target=self.schedule_zombie_spawn, daemon=True)
-        thread.start()
+        self.is_running = True
+        for channel in self.channel_list:
+            game_thread = threading.Thread(target=self.start_game_loop, args=(channel,))
+            self.game_threads.append(game_thread)
+            game_thread.start()
 
-    def on_disconnect(self, c, e):
-        time.sleep(5)
-        self.jump_server()
+    def start_game_loop(self, channel):
+        channel_lower = channel.lower()
+        while self.is_connected() and self.is_running:
+            current_time = time.time()
+            if current_time - self.channel_last_cleanup[channel_lower] >= 600:
+                self.cleanup_zombies(channel_lower)
+                self.channel_last_cleanup[channel_lower] = current_time
 
-    def schedule_zombie_spawn(self):
-        while self.is_connected():
-            self.spawn_zombie()
-            time.sleep(random.randint(7200, 14400))
-
-    def spawn_zombie(self):
-        if self.is_connected():
-            zombie_id = random.randint(1000, 9999)
-            self.zombies[zombie_id] = time.time()
-            self.send_message(f"A wild zombie (ID: {zombie_id}) appears! Type '!shoot {zombie_id}' to kill it!")
-
-    def load_scores(self):
-        if os.path.exists('zombies.json'):
-            with open('zombies.json', 'r') as file:
-                return json.load(file)
-        else:
-            return {}
-
-    def save_scores(self):
-        with open('zombies.json', 'w') as file:
-            json.dump(self.scores, file)
+            self.save_zombies_state()
+            self.spawn_zombie(channel_lower)
+            time.sleep(random.randint(1800, 3600))
 
     def on_pubmsg(self, c, e):
         message = e.arguments[0].strip()
-        if message.startswith("!shoot"):
-            self.handle_shooting(c, e)
-        elif message.startswith("!reload"):
+        parts = message.split()
+        if not parts:
+            return
+
+        command = parts[0].lower()
+
+        if command == "!shoot":
+            self.handle_shooting(c, e, parts)
+        elif command == "!kills":
+            self.print_scores(c, e)
+        elif command == "!reload":
             self.handle_reloading(c, e)
-        elif message.startswith("!scores"):
-            self.print_scores()
 
-    def handle_shooting(self, c, e):
-        parts = e.arguments[0].split()
-        if len(parts) != 2 or parts[0] != "!shoot":
-            return
+    def on_disconnect(self, c, e):
+        self.stop_game_loop()
+        self.jump_server()
 
+    def spawn_zombie(self, channel):
+        if self.is_connected():
+            zombie_id = random.randint(1000, 9999)
+            if channel not in self.zombies:
+                self.zombies[channel] = {}
+            if random.random() < self.boss_chance:
+                self.zombies[channel][zombie_id] = {"spawn_time": time.time(), "health": self.boss_health, "is_boss": True}
+                self.connection.privmsg(channel, f"A boss zombie (ID: {zombie_id}) appears! It takes {self.boss_health} bullets to kill. Type '!shoot {zombie_id}' to shoot it!")
+            else:
+                self.zombies[channel][zombie_id] = {"spawn_time": time.time(), "health": 1, "is_boss": False}
+                self.connection.privmsg(channel, f"A wild zombie (ID: {zombie_id}) appears! Type '!shoot {zombie_id}' to kill it!")
+
+    def handle_shooting(self, c, e, parts):
         user = e.source.nick
-        if self.bullets.get(user, 0) <= 0:
-            self.send_message(f"{user}, you're out of bullets! Use !reload to reload your gun.")
-            return
+        channel = e.target
 
-        self.bullets[user] -= 1
+        if self.bullets.get(user) == "exploded":
+            c.privmsg(channel, f"Oh no, {user}! Your gun exploded! Type '!reload' to repair your gun.")
+            return
 
         try:
             zombie_id = int(parts[1])
-            if zombie_id in self.zombies:
-                shot_time = time.time() - self.zombies[zombie_id]
-                del self.zombies[zombie_id]
-                self.scores[user] = self.scores.get(user, 0) + 1
+            if user not in self.bullets or self.bullets[user] <= 0:
+                c.privmsg(channel, f"{user}, you are out of bullets. Type '!reload' to reload your gun.")
+                return
+
+            self.bullets[user] -= 1
+
+            if random.random() < self.explosion_chance:
+                self.bullets[user] = "exploded"
+                c.privmsg(channel, f"Oh no, {user}! Your gun exploded!")
+                self.scores[user] = max(self.scores.get(user, 0) - 2, 0)
+                c.privmsg(channel, f"{user}, due to the gun explosion you've lost 2 kills. Your current score is now {self.scores[user]}")
                 self.save_scores()
-                self.send_message(f"Nice shot, {user}! You killed zombie {zombie_id} in {shot_time:.2f} seconds!")
             else:
-                self.send_message(f"Zombie {zombie_id} is not found. Keep an eye on the channel for more zombies!")
+                channel_lower = channel.lower()
+                if zombie_id in self.zombies.get(channel_lower, {}):
+                    zombie = self.zombies[channel_lower][zombie_id]
+                    if "health" in zombie:
+                        if zombie["is_boss"]:
+                            if zombie["health"] > 1:
+                                zombie["health"] -= 1
+                                c.privmsg(channel, f"Nice shot, {user}! You hit boss zombie {zombie_id}. It now has {zombie['health']} health left.")
+                            else:
+                                shot_time = time.time() - zombie["spawn_time"]
+                                del self.zombies[channel_lower][zombie_id]
+                                self.scores[user] = self.scores.get(user, 0) + 1
+                                c.privmsg(channel, f"Great job, {user}! You killed boss zombie {zombie_id} in {shot_time:.2f} seconds! Your current score is now {self.scores[user]}.")
+                                self.save_scores()
+                        else:
+                            shot_time = time.time() - zombie["spawn_time"]
+                            del self.zombies[channel_lower][zombie_id]
+                            self.scores[user] = self.scores.get(user, 0) + 1
+                            c.privmsg(channel, f"Nice shot, {user}! You killed zombie {zombie_id} in {shot_time:.2f} seconds! Your current score is now {self.scores[user]}.")
+                            self.save_scores()
+                    else:
+                        c.privmsg(channel, f"Zombie {zombie_id} is not found. Keep an eye on the channel for more zombies!")
+                else:
+                    c.privmsg(channel, f"Zombie {zombie_id} is not found. Keep an eye on the channel for more zombies!")
+
         except ValueError:
-            self.send_message("Invalid command format. Usage: !shoot <zombie_id>")
+            c.privmsg(channel, "Invalid command format. Usage: !shoot <zombie_id>")
 
     def handle_reloading(self, c, e):
         user = e.source.nick
-        self.bullets[user] = 4
-        self.send_message(f"{user}, your gun is reloaded and ready to shoot!")
+        channel = e.target
+        self.bullets[user] = 5
+        if self.bullets.get(user) == "exploded":
+            c.privmsg(channel, f"{user}, your gun has been fixed and reloaded!")
+        else:
+            c.privmsg(channel, f"{user}, your gun is reloaded!")
 
-    def send_message(self, message):
-        if self.is_connected():
-            self.connection.privmsg(self.channel, message)
-
-    def print_scores(self):
+    def print_scores(self, c, e):
+        user = e.source.nick
         if self.is_connected():
             if self.scores:
-                self.send_message("Current Scores:")
-                for user, score in self.scores.items():
-                    self.send_message(f"{user}: {score}")
+                c.notice(user, "Current Scores:")
+                for scorer, score in self.scores.items():
+                    c.notice(user, f"{scorer}: {score}")
             else:
-                self.send_message("No scores yet. Keep shooting zombies!")
+                c.notice(user, "No scores yet. Keep shooting zombies!")
+
+    def save_scores(self):
+        with open("killz", "w") as f:
+            json.dump({'scores': self.scores, 'bullets': self.bullets}, f)
+
+    def cleanup_zombies(self, channel):
+        if channel not in self.zombies:
+            return
+
+        current_time = time.time()
+        zombies = self.zombies[channel]
+        zombies_to_remove = [zombie_id for zombie_id, zombie in zombies.items() if current_time - zombie["spawn_time"] > 600]
+        for zombie_id in zombies_to_remove:
+            del zombies[zombie_id]
+
+    def save_zombies_state(self):
+        with open("zombies_state.json", "w") as f:
+            json.dump(self.zombies, f)
+
+    def load_zombies_state(self):
+        try:
+            with open("zombies_state.json", "r") as f:
+                data = json.load(f)
+                self.zombies = data
+        except FileNotFoundError:
+            pass
+
+    def stop_game_loop(self):
+        self.is_running = False
+        for game_thread in self.game_threads:
+            game_thread.join()
+
+    def run(self):
+        self.load_zombies_state()
+        super().start()
 
 def main():
     server = "irc.twistednet.org"
-    channel = "#dev"
-    nickname = "ZombieBot"
+    channel_list = ["#twisted", "#dev"]
+    nickname = "Zombie"
 
-    bot = ZombieBot(channel, nickname, server)
-    bot.start()
+    bot = ZombieBot(channel_list, nickname, server)
+    bot.run()
 
 if __name__ == "__main__":
     main()
